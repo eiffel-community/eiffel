d1b183f1c233e8365181517e8cc9e732f3b4a80b
Daniel Stahl <daniel.stahl@ericsson.com>
2017-04-04 09:41:19 +0200

    Updated license notice of .py files.

--- examples/validate.py
+++ examples/validate.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 # Copyright 2017 Ericsson AB.
+# For a full list of individual contributors, please see the commit history.
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
